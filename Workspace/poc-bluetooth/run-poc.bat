c2b0b1a8bf806ae52b667bb6ca146e594b93d9d3
Ricardo Artur Staroski <ricardo.staroski@gmail.com>
2016-09-30 20:29:56 -0300

    removidos projetos não mais utilizados

--- Workspace/poc-bluetooth/run-poc.bat
+++
@@ -1,5 +0,0 @@
-javac -classpath ".\arm-runtime-libs;.\arm-runtime-libs\bluecove-2.1.1-SNAPSHOT.jar;.\bluecove-gpl-2.1.1-SNAPSHOT.jar" -sourcepath .\src -d .\bin .\src\*.java
-
-java -classpath ".\bin;.\arm-runtime-libs;.\arm-runtime-libs\bluecove-2.1.1-SNAPSHOT.jar;.\bluecove-gpl-2.1.1-SNAPSHOT.jar" TesteJanela
-
-
--- Workspace/poc-bluetooth/src/br/com/staroski/obdjrp/obd2/OBD2Listener.java
+++
@@ -1,8 +0,0 @@
-package br.com.staroski.obdjrp.obd2;
-
-import java.util.List;
-
-public interface OBD2Listener {
-
-	public void onDataReceived(List<OBD2Data> data);
-}
